74ebf12e1810de4c27a6a1185f5497801d7ba1a1
Sahil Rana <sahilrana27582@gmail.com>
2025-01-07 09:19:09 +0530

    Leetcode Find Mirror Score Of String

---
+++ Find_Mirror_Score_String.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long calculateScore(string s) {
+
+        long long ans = 0;
+
+        unordered_map<char, stack<int>> mpp;
+
+        for(int i=0;i<s.size(); i++){
+            char ch = char(219 - int(s[i]));
+            if(mpp.find(ch) == mpp.end() || mpp[ch].empty()){
+                mpp[s[i]].push(i);
+            }else{
+                ans += i - mpp[ch].top();
+                mpp[ch].pop();
+            }
+        }
+
+        return ans;
+        
+    }
+};
