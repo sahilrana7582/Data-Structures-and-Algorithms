f75617edfbef9c0f768524c414ea59ec6bab1035
Sahil Rana <sahilrana27582@gmail.com>
2025-01-12 10:34:06 +0530

    Palidrome Partinioning 4 For Loops

--- Dynamic Programming/PALIDROME_PARTIONING_4.cpp
+++ Dynamic Programming/PALIDROME_PARTIONING_4.cpp
@@ -45,3 +45,39 @@ public:
     }
 };
 // 2nd Solution
+
+
+
+
+class Solution {
+public:
+    bool isPali(int st, int en, string& s) {
+        while (st < en) {
+            if (s[st] != s[en]) {
+                return false;
+            }
+
+            st++;
+            en--;
+        }
+        return true;
+    }
+    bool checkPartitioning(string s) {
+
+        int n = s.size();
+
+        for (int i = 0; i < n - 2; i++) {
+            if (isPali(0, i, s)) {
+                for (int j = i + 1; j < n - 1; j++) {
+                    if (isPali(i + 1, j, s)) {
+                        if (isPali(j + 1, n - 1, s)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+};
