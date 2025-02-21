b98b04ee8b5281cb73d5ccba04145e0195e9412e
Sahil Rana <sahilrana27582@gmail.com>
2025-02-21 08:51:20 +0530

    Find the element appear only once

---
+++ Bit-Manipulation/Find_Single_Number2.cpp
@@ -0,0 +1,21 @@
+/*------ Using MAP  ------*/
+class Solution {
+    public:
+        int singleNumber(vector<int>& nums) {
+            unordered_map<int, int> mpp;
+    
+            for(int in : nums){
+                mpp[in]++;
+            }
+    
+            for(auto &pair : mpp){
+                if(pair.second == 1){
+                    return pair.first;
+                }
+            }
+    
+    
+            return -1;
+        }
+    }
+};
