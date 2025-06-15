d0ce2cece04b79d2a4621de33b318e97c3de8f3e
Sahil Rana <sahilrana27582@gmail.com>
2025-06-15 10:13:02 +0530

    Max Diff By Changing Ele | Greedy Approach

--- Greedy/Max_Diff_From_Changing_Integers.cpp
+++ Greedy/Max_Diff_From_Changing_Integers.cpp
@@ -26,3 +26,44 @@ class Solution {
             return maxVal - minVal;
         }
     };
+
+
+
+
+    // ---------------- GREEDY --------------------
+    class Solution {
+        public:
+            int maxDiff(int num) {
+                string s = to_string(num);
+                int maxVal = num, minVal = num;
+        
+                for (char x = '0'; x <= '9'; x++) {
+                    string t = s;
+                    char y = (x == t[0]) ? '1' : '0'; 
+        
+                    for (char &ch : t) {
+                        if (ch == x) ch = y;
+                    }
+        
+                    if (t[0] == '0') continue;  
+        
+                    int val = stoi(t);
+                    minVal = min(minVal, val);
+                }
+        
+                for (char x = '0'; x <= '9'; x++) {
+                    string t = s;
+                    for (char &ch : t) {
+                        if (ch == x) ch = '9';
+                    }
+        
+                    if (t[0] == '0') continue;
+        
+                    int val = stoi(t);
+                    maxVal = max(maxVal, val);
+                }
+        
+                return maxVal - minVal;
+            }
+        };
+        
